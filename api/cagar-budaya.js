06b9c1f3654043a7b2c6bc7e9cd58dc4413a0846
Zainal21 <Zainalarifin080718@gmail.com>
2022-01-09 15:33:04 +0700

    change req query

--- api/cagar-budaya.js
+++ api/cagar-budaya.js
@@ -4,14 +4,14 @@ const fs = require('fs').promises
 module.exports = async (req, res) => {
   const {
     kode_wilayah
-  } = req.params
+  } = req.query
   if (!kode_wilayah) {
     res.status(422).send({
       status: 422,
       message: "please input region code"
     });
   } else {
-    const fileJson = path.join(__dirname, '..', 'data/', `${kode_wilayah}.json`)
+    const fileJson = path.join(__dirname, '..', 'data/', 'kode_wilayah.json')
     const data = await fs.readFile(fileJson, 'utf-8')
     const JsonResult = JSON.parse(data)
     res.status(200).send(JsonResult)
